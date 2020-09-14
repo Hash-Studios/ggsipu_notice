867d5176c60830866191ef3978ae43a2a0371140
Abhay Maurya <maurya.abhay30@gmail.com>
2020-09-14 21:14:57 +0530

    added interstitial ad

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'package:another/keys.dart';
+import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
@@ -6,10 +8,10 @@ import 'package:another/ui/about.dart';
 import 'package:another/ui/noticetile.dart';
 import 'package:another/ui/themeswitch.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
 
   @override
@@ -17,6 +19,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Timer _timerForInter;
+
+  MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+    keywords: <String>['flutterio', 'beautiful apps'],
+    contentUrl: 'https://flutter.io',
+    childDirected: false,
+    testDevices: <String>["9033810B9AD198E151CFEC2CB5073E2B"],
+  );
+  InterstitialAd _interstitialAd;
+
+  InterstitialAd createInterstitialAd() {
+    return InterstitialAd(
+      adUnitId: InterstitialAd.testAdUnitId,
+      targetingInfo: targetingInfo,
+      listener: (MobileAdEvent event) {
+        print("InterstitialAd event $event");
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    FirebaseAdMob.instance.initialize(appId: appId);
+    _timerForInter = Timer.periodic(Duration(seconds: 30), (result) {
+      _interstitialAd = createInterstitialAd()
+        ..load()
+        ..show();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timerForInter.cancel();
+    _interstitialAd.dispose();
+    super.dispose();
+  }
+
   final databaseReference =
       FirebaseDatabase.instance.reference().child("notices");
   List lists = [];
