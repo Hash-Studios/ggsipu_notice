0d5e4475e5342e66b30f9e71c1e119f469a0a329
Abhay Maurya <maurya.abhay30@gmail.com>
2021-04-18 11:36:04 +0530

    refactor directories

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:ggsipu_notice/keys.dart';
-import 'package:ggsipu_notice/screens/home.dart';
+import 'package:ggsipu_notice/ui/pages/home.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
