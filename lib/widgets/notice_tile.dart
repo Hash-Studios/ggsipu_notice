7f1426382a94c9d636da469d0d0d9807aecda039
LiquidatorCoder <maurya.abhay30@gmail.com>
2021-11-18 17:00:14 +0530

    add slidable options

--- lib/widgets/notice_tile.dart
+++ lib/widgets/notice_tile.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:ip_notices/models/notice.dart';
 import 'package:ip_notices/services/locator.dart';
@@ -40,178 +41,78 @@ class NoticeTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final SlidableController slidableController = SlidableController();
     final _themeService = locator<ThemeService>();
-    return CupertinoContextMenu(
-      key: ValueKey(document?.title ?? ''),
-      previewBuilder: (context, animation, child) => Card(
-        color: _themeService.background(context),
-        margin: const EdgeInsets.all(0),
-        elevation: 0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        child: ListTile(
-          isThreeLine: (document?.college ?? '').toUpperCase().trim() != "",
-          enableFeedback: true,
-          onTap: () {
-            String link = "http://www.ipu.ac.in${document?.url}";
-            _launchURL(link);
-          },
-          contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-          leading: CircleAvatar(
-              backgroundColor: HexColor.fromHex(
-                  document?.color ?? Colors.grey.withOpacity(0.3).toHex()),
-              child: Text(
-                document?.title[0] ?? '',
-                style: TextStyle(
-                  color: _themeService.onBackground(context).withOpacity(0.7),
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16,
-                ),
-              )),
-          title: Text(
-            (document?.college ?? '').toUpperCase().trim() != ""
-                ? (document?.college ?? '').toUpperCase().trim()
-                : document?.title ?? '',
-            maxLines: 10,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              color: _themeService.onBackground(context),
-              fontWeight: (document?.college ?? '').toUpperCase().trim() == ""
-                  ? FontWeight.normal
-                  : FontWeight.bold,
-              fontSize: 14,
+    return Slidable(
+      controller: slidableController,
+      actionPane: const SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      closeOnScroll: true,
+      actions: <Widget>[
+        IconSlideAction(
+          caption: 'Download',
+          color: _themeService.accent(context),
+          foregroundColor: _themeService.onAccent(context),
+          iconWidget: Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Icon(
+              CupertinoIcons.cloud_download,
+              color: _themeService.onAccent(context),
             ),
           ),
-          subtitle: (document?.college ?? '').toUpperCase().trim() == ""
-              ? null
-              : Text(
-                  document?.title ?? '',
-                  maxLines: 10,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    color: _themeService.onBackground(context),
-                    fontSize: 14,
-                  ),
-                ),
-          trailing: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                timeago
-                        .format(DateTime.parse(
-                            document?.createdAt.toString() ?? '0'))
-                        .contains('day')
-                    ? "${document?.date.split('-')[0]} ${DateFormat('MMM').format(DateTime(0, int.parse(document?.date.split('-')[1] ?? '0')))}"
-                    : timeago
-                        .format(DateTime.parse(
-                            document?.createdAt.toString() ?? '0'))
-                        .replaceAll('about', '')
-                        .replaceAll('hour', 'hr')
-                        .replaceAll('minute', 'min')
-                        .replaceAll('ago', '')
-                        .replaceAll('an', '1')
-                        .trim(),
-                textAlign: TextAlign.end,
-                style: TextStyle(
-                  color: _themeService.onBackground(context).withOpacity(0.8),
-                  fontSize: 12,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-              // const SizedBox(
-              //   height: 6,
-              // ),
-              // Text(
-              //   (document?.college ?? '').toUpperCase().trim(),
-              //   textAlign: TextAlign.end,
-              //   style: TextStyle(
-              //     color: Colors.black.withOpacity(0.6),
-              //     fontSize: 12,
-              //     fontWeight: FontWeight.w300,
-              //   ),
-              // ),
-            ],
-          ),
+          onTap: () async {
+            if (download) {
+              var status = await Permission.storage.status;
+              if (!status.isGranted) {
+                await Permission.storage.request();
+              }
+              String link = "http://www.ipu.ac.in${document?.url}";
+              String _localPath = (await _findLocalPath()) + '/Notices';
+              final savedDir = Directory(_localPath);
+              bool hasExisted = await savedDir.exists();
+              if (!hasExisted) {
+                savedDir.create();
+              }
+              final f = Random();
+              String name = "";
+              for (int i = 0; i < 10; i++) {
+                name = name + f.nextInt(9).toString();
+              }
+              final taskId = await FlutterDownloader.enqueue(
+                url: link,
+                fileName:
+                    '${document?.title.toString().replaceAll("/", "")} $name.pdf',
+                savedDir: _localPath,
+                showNotification: true,
+                openFileFromNotification: true,
+              );
+              logger.d("Task Id - $taskId");
+              logger.d("Local Path - $_localPath");
+            } else {
+              String link = "http://www.ipu.ac.in${document?.url}";
+              _launchURL(link);
+            }
+          },
         ),
-      ),
-      actions: download
-          ? [
-              CupertinoContextMenuAction(
-                child: const Text("View Notice"),
-                isDefaultAction: true,
-                trailingIcon: CupertinoIcons.doc_text,
-                onPressed: () {
-                  Navigator.pop(context);
-                  String link = "http://www.ipu.ac.in${document?.url}";
-                  _launchURL(link);
-                },
-              ),
-              CupertinoContextMenuAction(
-                child: const Text("Download"),
-                trailingIcon: CupertinoIcons.cloud_download,
-                onPressed: () async {
-                  Navigator.pop(context);
-                  var status = await Permission.storage.status;
-                  if (!status.isGranted) {
-                    await Permission.storage.request();
-                  }
-                  String link = "http://www.ipu.ac.in${document?.url}";
-                  String _localPath = (await _findLocalPath()) + '/Notices';
-                  final savedDir = Directory(_localPath);
-                  bool hasExisted = await savedDir.exists();
-                  if (!hasExisted) {
-                    savedDir.create();
-                  }
-                  final f = Random();
-                  String name = "";
-                  for (int i = 0; i < 10; i++) {
-                    name = name + f.nextInt(9).toString();
-                  }
-                  final taskId = await FlutterDownloader.enqueue(
-                    url: link,
-                    fileName:
-                        '${document?.title.toString().replaceAll("/", "")} $name.pdf',
-                    savedDir: _localPath,
-                    showNotification: true,
-                    openFileFromNotification: true,
-                  );
-                  print(_localPath);
-                },
-              ),
-              CupertinoContextMenuAction(
-                child: const Text("Share"),
-                trailingIcon: CupertinoIcons.share,
-                onPressed: () {
-                  Navigator.pop(context);
-                  String link = "http://www.ipu.ac.in${document?.url}";
-                  Share.share("$link\n${document?.title}");
-                },
-              )
-            ]
-          : [
-              CupertinoContextMenuAction(
-                child: const Text("View Notice"),
-                isDefaultAction: true,
-                trailingIcon: CupertinoIcons.doc_text,
-                onPressed: () {
-                  Navigator.pop(context);
-                  String link = "http://www.ipu.ac.in${document?.url}";
-                  _launchURL(link);
-                },
-              ),
-              CupertinoContextMenuAction(
-                child: const Text("Share"),
-                trailingIcon: CupertinoIcons.share,
-                onPressed: () {
-                  Navigator.pop(context);
-                  String link = "http://www.ipu.ac.in${document?.url}";
-                  Share.share("$link\n${document?.title}");
-                },
-              )
-            ],
+      ],
+      secondaryActions: <Widget>[
+        IconSlideAction(
+          caption: 'Share',
+          color: _themeService.accent(context),
+          foregroundColor: _themeService.onAccent(context),
+          iconWidget: Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Icon(
+              CupertinoIcons.share,
+              color: _themeService.onAccent(context),
+            ),
+          ),
+          onTap: () {
+            String link = "http://www.ipu.ac.in${document?.url}";
+            Share.share("$link\n${document?.title}");
+          },
+        ),
+      ],
       child: Card(
         color: _themeService.background(context),
         margin: const EdgeInsets.all(0),
@@ -231,9 +132,6 @@ class NoticeTile extends StatelessWidget {
                 (document?.college ?? '').toUpperCase().trim().isNotEmpty,
             // || ((document?.tags ?? []).isNotEmpty),
             enableFeedback: true,
-            onLongPress: () {
-              logger.d("Long Press");
-            },
             onTap: () {
               String link = "http://www.ipu.ac.in${document?.url}";
               _launchURL(link);
